78b8493be5a04e17b6d528bc521a00a6dd2afd61
Yudha Agung <34732992+yudha-e@users.noreply.github.com>
2020-11-07 04:05:42 +0700

    set js

--- dev/balai-kirti/assets/js/app-detail.js
+++ dev/balai-kirti/assets/js/app-detail.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 var myParam = urlParams.get('koleksi');
 var selectedCollection = 1;
-if(!isNaN(myParam)) selectedCollection = parseInt(myParam);
+if(isNaN(myParam)) selectedCollection = parseInt(myParam);
 var data;
 var intViewportWidth = window.innerWidth;
 var numberElement = document.querySelector('#numberCollection');
