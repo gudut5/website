177fe9a24c0092353d0b4fef09efb1b6926aa4fd
Yudha Agung <34732992+yudha-e@users.noreply.github.com>
2020-11-12 02:28:41 +0700

    revert back

--- dev/balai-kirti/assets/js/app.js
+++ dev/balai-kirti/assets/js/app.js
@@ -85,48 +85,15 @@ function drawSlider(){
         indexCurrent = info.index+1;
         document.getElementById("numberCollection").innerHTML = indexCurrent;
     }       
-                                                       
-    document.addEventListener('mousemove', handleMouseMove, false);   
-    function handleMouseMove(){
+    document.addEventListener('touchend', handleTouchMove, false);                                                   
+    document.addEventListener('mousemove', handleTouchMove, false);                                          
+    function handleTouchMove(evt) {
         setTimeout(function(){ 
             var info = sliders['base'].getInfo(),
             indexCurrent = info.displayIndex;
             document.getElementById("numberCollection").innerHTML = indexCurrent;
-        }, 500);  
-    }
-    //document.addEventListener('touchend', handleTouchMove, false);
-    document.addEventListener('touchstart', handleTouchStart, false);        
-    document.addEventListener('touchmove', handleTouchMove, false);
-    var xDown = null;                                                        
-    var yDown = null;  
-    function getTouches(evt) {
-        return evt.touches ||             // browser API
-               evt.originalEvent.touches; // jQuery
-    }                                                     
-      
-    function handleTouchStart(evt) {
-        const firstTouch = getTouches(evt)[0];                                      
-        xDown = firstTouch.clientX;                                      
-        yDown = firstTouch.clientY;                                      
-    };                                          
-    function handleTouchMove(evt) {
-        if ( ! xDown || ! yDown ) {
-            return;
-        }
-    
-        var xUp = evt.touches[0].clientX;                                    
-        var yUp = evt.touches[0].clientY;
-    
-        var xDiff = xDown - xUp;
-        var yDiff = yDown - yUp;
-    
-        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-            setTimeout(function(){ 
-                var info = sliders['base'].getInfo(),
-                indexCurrent = info.displayIndex;
-                document.getElementById("numberCollection").innerHTML = indexCurrent;
-            }, 500);                   
-        }                                         
+        }, 500);
+                                                  
     };
 }
 function drawList(){
